31b952ad17cd5c002002916040513d92f4d11e4c
Nour kayyali <nourkayyali91@gmail.com>
2024-02-04 21:25:42 +0300

    created search routing for admin

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -20,4 +20,29 @@ return res.status(404).json({message:"No Item found"})
     res.status(500).json({ message: "Internal Server Error" });
     }
 }
-module.exports={removeItems};
+
+const searchItems=async(req,res)=>
+{
+    const {title,genre,minPrice, maxPrice}=req.query;
+    const query={};
+    if (title)  query.title ={ $regex: new RegExp(title, 'i') };//$regex: Provides regular expression capabilities for pattern matching strings in queries. i: Case-insensitive
+    if (genre) query.genre=genre;
+    if (minPrice && maxPrice) {
+        query.price = { $gte: minPrice, $lte: maxPrice };
+      }
+  try {
+    const items=await shopItemModel.find(query);
+    if (items.length>0){
+    res.status(200).json({message:"Search Result",items});
+    }
+    elsegit 
+    {
+         res.status(404).json({message:"No Items found"})
+    }
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error searching items' });
+  }
+}
+module.exports={removeItems,searchItems};
