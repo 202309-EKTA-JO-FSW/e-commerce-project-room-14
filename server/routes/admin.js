1bbf0927c72be88400280026ec2904f4390525a3
Hadeel Obaid <hadeelobaid71@gmail.com>
2024-02-05 23:20:27 +0300

    Admin route add

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,4 +4,5 @@ const adminController=require("../controllers/admin");
 
 router.delete("/removeItems",adminController.removeItems);
 router.get("/searchItems",adminController.searchItems);
+router.post('/addItem', adminController.addNewItem);
 module.exports=router;
