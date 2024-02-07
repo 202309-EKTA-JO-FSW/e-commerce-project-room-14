0a03e1584890b1d6e41f574ab3448cac54bbf133
Nour kayyali <nourkayyali91@gmail.com>
2024-02-07 22:26:14 +0300

    admin authentication

--- server/middleware/authenticateAdmin.js
+++ server/middleware/authenticateAdmin.js
@@ -1,1 +1,25 @@
-//code
+const jwt=require("jsonwebtoken");
+const Access_Token_Key = process.env.Access_Token_Key; // Access secret key
+const authenticateAdmin=async(req,res,next)=>
+{
+    const token =req.cookies.jwt;// Retrieve token from cookies
+
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' }); // No token provided
+    }
+    try {
+        const decoded = jwt.verify(token, Access_Token_Key); // Verify token authenticity
+    
+        if (!decoded.isAdmin) {
+          return res.status(403).json({ message: 'Forbidden: Admin access required' }); // Not an admin
+        }
+    
+        req.user = decoded; // Attach user information to the request object
+        next(); // Proceed to the endpoint
+      } catch (error) {
+        console.error(error); // Handle token-related errors
+        return res.status(401).json({ message: 'Unauthorized' }); // Invalid or expired token
+      }
+
+}
+module.exports={authenticateAdmin};
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/admin");
-
+const {authenticateAdmin}=require("../middleware/authenticateAdmin");
 router.post("/signin",adminController.signin);
-router.post("/new-admin",adminController.newAdmin);
+router.post("/new-admin",authenticateAdmin,adminController.newAdmin);
 router.post("/signout",adminController.signout);
-router.delete("/removeItems",adminController.removeItems);
-router.get("/searchItems",adminController.searchItems);
-router.post('/addItem', adminController.addNewItem);
-router.put('/items/:itemId', adminController.updateItemDetails);
-router.get("/customers",adminController.getCustomers);
-router.get('/orders', adminController.getOrders);
+router.delete("/removeItems",authenticateAdmin,adminController.removeItems);
+router.get("/searchItems",authenticateAdmin,adminController.searchItems);
+router.post('/addItem', authenticateAdmin,adminController.addNewItem);
+router.put('/items/:itemId',authenticateAdmin, adminController.updateItemDetails);
+router.get("/customers",authenticateAdmin,adminController.getCustomers);
+router.get('/orders', authenticateAdmin,adminController.getOrders);
 
 
 
